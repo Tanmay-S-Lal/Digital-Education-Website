80534f94dc6d2b731bfb1b9941d494d0e3681bfc
Tanmay S Lal <tanmayshibulal1234@gmail.com>
2023-09-26 20:12:59 +0530

    Firebase Deployed

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,17 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSENGER,
-  appId: process.env.REACT_APP_ID
+  apiKey: "AIzaSyCGw1ai-zgzJI_pUDlL4Dfh901fDgLxq24",
+  authDomain: "digital-education-website.firebaseapp.com",
+  projectId: "digital-education-website",
+  storageBucket: "digital-education-website.appspot.com",
+  messagingSenderId: "559686565896",
+  appId: "1:559686565896:web:d7a8e27e67cc67cb9851bd",
+  measurementId: "G-712X4261FC"
 };
 
 // Initialize Firebase
